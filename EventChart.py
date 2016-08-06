d4615ee5d6b82b27ed4667d0b717ce87421a8e88
Neal Kaviratna <Nkaviratna@gatech.edu>
2016-08-06 19:42:18 -0400

    Finished EventChart script

--- EventChart.py
+++ EventChart.py
@@ -1,7 +1,47 @@
+import facebook
+import util
+import datetime
+import humanize
+import re
+
 #---------------------- Event List ----------------------
-events = [  1787573954810921,   # Bad Manners 4
-            788861314549484,    # Battle above the clouds
-            924707064342438 ]   # NACL August
+event_ids = ['788861314549484', # Battle above the clouds
+            #'692430650895680',  # STS
+            '1787573954810921', # Bad Manners 4
+            '1216950281690985', # No Contest 8/20
+            '924707064342438',  # NACL August 8/27
+            '835712656564201',  # NACL September
+            '272904843091886',  # NACL October
+            ]  
 #--------------------------------------------------------
+secrets = open('graphapi.secret', 'r')
+at = util.get_fb_token(secrets.readline().strip(), secrets.readline().strip())
+graph = facebook.GraphAPI(access_token = at, version = '2.2')
 
+results = open('EventChart.result', 'w+')
+results.write("------------------- Upcoming Events -------------------\n---------------------------------------------------------------\n\n")
 
+csv = open('EventChart.csv', 'w+')
+csv.write("Name,Date,Addresses,Venue,Facebook Page\n")
+
+for event_id in event_ids:
+    event = graph.get_object( id = event_id)
+    
+    name = re.sub("\(.*\)","",event['name'])
+    print event['place']
+    
+    date = datetime.datetime.strptime(event['start_time'][:10], "%Y-%m-%d")
+    date = humanize.naturalday(date).title()
+    
+    city = event['place']['location']['city']
+    state = event['place']['location']['state']
+    address = event['place']['location']['street']
+    location = event['place']['name']
+    
+    url = "facebook.com/events/"+event_id
+    
+    results.write(date + " - " + name + " [" + city + ", " + state + "]\n"+url+"\n----------------------------------------------------------------\n")
+    csv.write(name+","+date+","+address+","+location+","+url+"\n")
+    
+results.close()
+csv.close()
---
+++ util.py
@@ -0,0 +1,10 @@
+import requests
+
+# Snagged from: http://stackoverflow.com/a/26844734
+def get_fb_token(app_id, app_secret):           
+    payload = {'grant_type': 'client_credentials', 'client_id': app_id, 'client_secret': app_secret}
+    file = requests.post('https://graph.facebook.com/oauth/access_token?', params = payload)
+    #print file.text #to test what the FB api responded with    
+    result = file.text.split("=")[1]
+    #print file.text #to test the TOKEN
+    return result
